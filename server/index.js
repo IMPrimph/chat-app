fa83fc2c9763794ad92419ef19f98deac36aed02
G Vishnu sai jaswanth <g.vishnu.sai.jaswanth@hp.com>
2022-07-15 18:00:32 +0530

    change env case

--- server/index.js
+++ server/index.js
@@ -24,7 +24,7 @@ app.use("/api/message", messageRoutes);
 
 const __dirname1 = path.resolve();
 
-if (process.env.NODE_ENV === "production") {
+if (process.env.NODE_ENV === "PRODUCTION") {
   app.use(express.static(path.join(__dirname1, "/client/build")));
 
   app.get("*", (req, res) => {
